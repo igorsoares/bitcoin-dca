39aa4fb8081415610598d22700c16eb98219608b
igorsoares <igor.hscuculha@gmail.com>
2025-12-05 13:58:51 -0300

    First commit - Starting cron script

---
+++ api_key.py
@@ -0,0 +1,11 @@
+import datetime
+import os
+from parameters import build_script_args
+
+
+
+
+if __name__ == '__main__':
+    arguments = build_script_args()
+    print("Starting script with the following parameters: " + str(arguments))
+    
